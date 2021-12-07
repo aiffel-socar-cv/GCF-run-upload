54e655aedfaa8377eac1d4cc3c6cccd7e7aaf276
Jieun Jeon <jieunjeon1023@gmail.com>
2021-12-07 14:52:46 +0900

    update handling the inferred images

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # This file contains all the code used in the codelab. 
 import sqlalchemy
+# from PIL import Image
+from google.cloud import storage
 
 # Depending on which database you are using, you'll set some variables differently. 
 # In this code we are inserting only one field with one value. 
@@ -14,6 +16,12 @@ db_password = "viai2021"
 
 driver_name = 'postgres+pg8000'
 query_string =  dict({"unix_sock": "/cloudsql/{}/.s.PGSQL.5432".format(connection_name)})
+
+sample_input_path = "./sample_images/input/img_0253.png"
+sample_output_dent_path = "./sample_images/output/output_0253_dent.png"
+sample_output_scratch_path = "./sample_images/output/output_0253_scratch.png"
+sample_output_spacing_path = "./sample_images/output/output_0253_scratch.png"
+
 
 # If the type of your table_field value is a string, surround it with double quotes.
 def run_upload(event, context):
@@ -41,9 +49,11 @@ def run_upload(event, context):
     # 1. Insert new row to the Cloud SQL
     insert(event['name'], event['timeCreated'])
     # 2. Hit the TorchServe inference API
-    predict(event['bucket'], event['name'])
-    # 3. Update Cloud SQL - mark as inferred, and path for the masks
-    # update()
+    predictions = predict(event['bucket'], event['name'])
+    # 3. Update Cloud Storage - upload masked images to 'images-inferre'
+    update_gcs(predictions)
+    # 4. Update Cloud SQL - mark as inferred, and path for the masks
+    update_psql(predictions)
 
 def insert(path_original, created_on):
     # request_json = request.get_json()
@@ -76,14 +86,71 @@ def insert(path_original, created_on):
 
 def predict(bucket, original_path):
     print('Predict: ', bucket + "/" + original_path)
+    # TODO: integrate with AI Prediction url
+    print("Predict_test: ",sample_output_dent_path)
 
-def update():
+    # return sample images
+    res = {
+        "path_original" : original_path,
+        "output_dent_path" : sample_output_dent_path,
+        "output_scratch_path" : sample_output_scratch_path,
+        "output_spacing_path" : sample_output_spacing_path,
+        "destination_blob_name_dent" : "masks-dent/" + original_path,
+        "destination_blob_name_scratch" : "masks-scratch/" + original_path,
+        "destination_blob_name_spacing" : "masks-spacing/" + original_path
+    }
+    return res
+
+def update_gcs(predictions):
+    storage_client = storage.Client.from_service_account_json('./aiffel-gn-3-1dcb97107eaa.json')
+
+    bucket_name = "images-inferred"
+    output_dent_path = predictions.output_dent_path
+    output_scratch_path = predictions.output_scratch_path
+    output_spacing_path = predictions.output_spacing_path
+    destination_blob_name_dent = predictions.destination_blob_name_dent
+    destination_blob_name_scratch = predictions.destination_blob_name_scratch
+    destination_blob_name_spacing = predictions.destination_blob_name_spacing
+
+    def upload_blob(bucket_name, source_file_name, destination_blob_name):
+        """Uploads a file to the bucket."""
+        bucket = storage_client.bucket(bucket_name)
+        blob = bucket.blob(destination_blob_name)
+
+        blob.upload_from_file(source_file_name)
+
+        print(
+            "File {} uploaded to {}.".format(
+                source_file_name, destination_blob_name
+            )
+        )
+
+    upload_blob(bucket_name, output_dent_path, destination_blob_name_dent)
+    upload_blob(bucket_name, output_scratch_path, destination_blob_name_scratch)
+    upload_blob(bucket_name, output_spacing_path, destination_blob_name_spacing)
+
+
+def update_psql(predictions):
     # request_json = request.get_json()
-    table_field = "path_original"
-    table_field_value = "GCF-test1"
+    # table_field = "path_original"
+    table_field_key = "path_original"
+    table_field_dent = "path_inference_dent"
+    table_field_scratch = "path_inference_scratch"
+    table_field_spacing = "path_inference_spacing"
+
+
+    # table_field_value = "GCF-test1"
     
-    stmt = sqlalchemy.text('insert into {} ({}) values ({})'.format(table_name, table_field, table_field_value))
-    
+    # TODO: update field with input_path as path_original
+    stmt = sqlalchemy.text('update {} set {}={}, {}={}, {}={} where {}={}'.format(table_name, table_field_dent, predictions.destination_blob_name_dent, table_field_scratch), predictions.destination_blob_name_scratch, table_field_spacing, predictions.destination_blob_name_spacing, table_field_key, predictions.path_original)
+    """
+    update {table_name} 
+      set {table_field_dent}={predictions.destination_blob_name_dent}, {table_field_scratch}={predictions.destination_blob_name_scratch}, {table_field_spacing}={predictions.destination_blob_name_spacing}
+      where {table_field_key}={predictions.path_original}
+    """
+    # stmt_1 = f"update {table_name} set {table_field_dent}={predictions.destination_blob_name_dent}, {table_field_scratch}={predictions.destination_blob_name_scratch}, {table_field_spacing}={predictions.destination_blob_name_spacing} where {table_field_key}={predictions.path_original}"
+    # print("update {table_name} set {table_field_dent}={predictions.destination_blob_name_dent}, {table_field_scratch}={predictions.destination_blob_name_scratch}, {table_field_spacing}={predictions.destination_blob_name_spacing} where {table_field_key}={predictions.path_original}")
+
     db = sqlalchemy.create_engine(
       sqlalchemy.engine.url.URL(
         drivername=driver_name,
@@ -103,3 +170,8 @@ def update():
     except Exception as e:
         print('Error: {}'.format(str(e)))
     print('success')
+
+
+
+#Test
+predict("images-original", "img_0253.png")
