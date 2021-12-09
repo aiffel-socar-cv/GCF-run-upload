27a8c73b6bca76be4ee1cca79fe11b8ce5157ae4
jieunjeon <jieunjeon1023@gmail.com>
2021-12-09 09:09:10 +0900

    fix error in query

--- main.py
+++ main.py
@@ -150,7 +150,7 @@ def update_psql(predictions, inferenced_on):
     # table_field_value = "GCF-test1"
     
     # TODO: update field with input_path as path_original
-    stmt = sqlalchemy.text('update {} set {}=\'{}\', {}=\'{}\', {}=\'{}\', {}=\'{}\', {}=\'{}\' where {}=\'{}\''.format(table_name, table_field_dent, predictions["destination_blob_name_dent"] , table_field_scratch, predictions["destination_blob_name_scratch"], table_field_spacing, predictions["destination_blob_name_spacing"], table_field_inferenced, inferenced_on, table_field_conf_score, predictions["conf_score"], table_field_key, predictions["path_original"]))
+    stmt = sqlalchemy.text('update {} set {}=\'{}\', {}=\'{}\', {}=\'{}\', {}=\'{}\', {}={} where {}=\'{}\''.format(table_name, table_field_dent, predictions["destination_blob_name_dent"] , table_field_scratch, predictions["destination_blob_name_scratch"], table_field_spacing, predictions["destination_blob_name_spacing"], table_field_inferenced, inferenced_on, table_field_conf_score, predictions["conf_score"], table_field_key, predictions["path_original"]))
     """
     update {table_name} 
       set {table_field_dent}={predictions.destination_blob_name_dent}, {table_field_scratch}={predictions.destination_blob_name_scratch}, {table_field_spacing}={predictions.destination_blob_name_spacing}
